4a27f0e55440d5c29035ea3b99167bd37782b7ce
kennedy-ak <akogokennedy@gmail.com>
2024-05-31 06:14:45 +0000

    static files

--- FileNest/settings.py
+++ FileNest/settings.py
@@ -137,13 +137,19 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-
+import os
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-import os
+
+# Directory where collected static files will be stored
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Additional locations the staticfiles app will traverse
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
