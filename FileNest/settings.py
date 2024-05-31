e46f3e2789966d91de12de9952bac7a498a9337b
kennedy-ak <akogokennedy@gmail.com>
2024-05-31 06:26:45 +0000

    static files

--- FileNest/settings.py
+++ FileNest/settings.py
@@ -147,11 +147,6 @@ import os
 
 STATIC_URL = "static/"
 
-# Directory where collected static files will be stored
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# Additional locations the staticfiles app will traverse
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
